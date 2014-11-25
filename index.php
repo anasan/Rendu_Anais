f9678f7e4b860af46045803647af19f5e8c9bea9
Anais SANCHEZ <anais.sanchezzzz@gmail.com>
2014-11-25 21:28:21 +0100

    commit index finished

--- index.php
+++ index.php
@@ -2,34 +2,3 @@
 
 require __DIR__.'/vendor/autoload.php';
 
-/**
- * Variable declaration
- */
-$boolean = true;
-
-/**
- * Int Validator
-*/
-//echo \WEB2\Validator\IntegerValidator::equal(8, 8);
-//echo \WEB2\Validator\IntegerValidator::superior(9, 8);
-//echo \WEB2\Validator\IntegerValidator::lower(6, 8);
-//echo \WEB2\Validator\IntegerValidator::between(8, 8, 10);
-//echo \WEB2\Validator\IntegerValidator::negative(-2);
-//echo \WEB2\Validator\IntegerValidator::positive(1);
-
-/**
- * String Validator
- */
-//echo \WEB2\Validator\StringValidator::stringEqual('abc',3);
-//echo \WEB2\Validator\StringValidator::stringSuperior('abc',2);
-//echo \WEB2\Validator\StringValidator::stringInferior('abc',4);
-//echo \WEB2\Validator\StringValidator::stringBetween('abc',1,5);
-//echo \WEB2\Validator\StringValidator::stringWhiteSpace('abc');
-//echo \WEB2\Validator\StringValidator::stringNoWhiteSpace('abc');
-
-/**
- * Boolean Validator
- */
-//echo \WEB2\Validator\BooleanValidator::isTrue($boolean);
-
-
